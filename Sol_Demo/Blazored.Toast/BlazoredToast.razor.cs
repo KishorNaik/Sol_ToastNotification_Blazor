658b32d42410fe280e4025d9b965ad78f993b538
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-07 17:40:52 +0530

    Code Commit

---
+++ Sol_Demo/Blazored.Toast/BlazoredToast.razor.cs
@@ -0,0 +1,19 @@
+﻿using Blazored.Toast.Configuration;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Blazored.Toast
+{
+    public partial class BlazoredToast
+    {
+        [CascadingParameter] private BlazoredToasts ToastsContainer { get; set; }
+
+        [Parameter] public Guid ToastId { get; set; }
+        [Parameter] public ToastSettings ToastSettings { get; set; }
+
+        private void Close()
+        {
+            ToastsContainer.RemoveToast(ToastId);
+        }
+    }
+}
---
+++ Sol_Demo/Blazored.Toast/Configuration/ToastSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazored.Toast.Configuration
+{
+    public class ToastSettings
+    {
+        public ToastSettings(string heading, RenderFragment message, string baseClass, string additionalClasses, string iconClass)
+        {
+            Heading = heading;
+            Message = message;
+            BaseClass = baseClass;
+            AdditionalClasses = additionalClasses;
+            IconClass = iconClass;
+        }
+
+        public string Heading { get; set; }
+        public RenderFragment Message { get; set; }
+        public string BaseClass { get; set; }
+        public string AdditionalClasses { get; set; }
+        public string IconClass { get; set; }
+    }
+}
